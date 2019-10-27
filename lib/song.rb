59eaead00c154dcd9352ba3fd0cabb9657eb588a
Randy Steele <steelerx155@gmail.com>
2019-10-27 02:12:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
+
 # require 'pry'
 require 'pry'
 class Song
   @@count = 0 
   @@artists = []
+  @@genres = []
+  
   
     attr_accessor :name,  :artist,  :genre
     
@@ -21,46 +24,10 @@ class Song
   
     @@artists << artist
     
-end
+    @@genres << genre
+    
+
+   
+  
   end
-
-
-
-# class Song
-#   @@count = 0 
-  
-#     attr_accessor :name
-#     attr_accessor :artist
-#     attr_accessor :genre
-    
-#     def initialize(name, artist, genre)
-      
-      
-  
-#     @@count += 1 
-    
-#     @name=name
-    
-    
-#     @artist=artist
-    
-#     # binding.pry
-#     @genre = genre
-  
-#     @@artists = [] << artist
-    
-#   # def initialize(name, artist, genre)
-    
-# end
-#   end
-#     # @@count += 1 
-    
-#     # @name=name
-    
-    
-#     # @artist=artist
-    
-#     # # binding.pry
-#     # @genre = genre
-  
-#     # @@artists = [] << artist
+end  
